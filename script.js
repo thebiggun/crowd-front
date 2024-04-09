9b3f4f3be69cb4b3995ec0b8c913e2c169e6135f
Astitva Agarwal <77670353+PeaceOut007@users.noreply.github.com>
2024-04-09 15:11:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+// function toggleMenu()
+// {
+//     const menu= document.querySelector('.menu-links');
+//     const icon = document.querySelector('.hamburger-icon');
+//     menu.classList.toggle("open");
+//     icon.classList.toggle("open");
+// }
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
